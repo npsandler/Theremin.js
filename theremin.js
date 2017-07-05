f284ca194317888e10c219b95557f4dcaaf0e421
Nathaniel Sandler <npsandler@gmail.com>
2017-07-05 15:56:56 -0400

    push before branch

--- theremin.js
+++ theremin.js
@@ -16,7 +16,7 @@ document.addEventListener('mousedown', function(e){capture(e)});
 document.addEventListener('mousemove',function(e){drag(e)});
 document.addEventListener('mouseup',function(e){release(e)});
 
-// Start the Note
+
 function capture(e){
   e.preventDefault();
   active = true;
@@ -24,26 +24,27 @@ function capture(e){
   freq = ~~(1000*(1-((e.clientY||e.touches[0].clientY)/window.innerHeight)));
   osc.frequency.value = freq;
   gain.gain.value = volume;
-  output.innerHTML = 'Frequency = '+freq+'hz, Volume = '+~~(volume*100)+'%';
+  // output.innerHTML = 'Frequency = '+freq+'hz, Volume = '+~~(volume*100)+'%';
 }
-// Update the properties of the note
+
 function drag(e){
   e.preventDefault()
+  // var width = document.getElementById('canvas').offsetWidth;
   if (active) {
     volume = ~~((e.clientX||e.touches[0].clientX)/window.innerWidth*100)/100;
     freq = ~~(1000*(1-((e.clientY||e.touches[0].clientY)/window.innerHeight)));
     osc.frequency.value = freq;
     gain.gain.value = volume;
-    output.innerHTML = 'Frequency = '+freq+'hz, Volume = '+~~(volume*100)+'%';
+    // output.innerHTML = 'Frequency = '+freq+'hz, Volume = '+~~(volume*100)+'%';
   }
 }
-// Release The Note
+
 function release(e){
   active = false;
   gain.gain.value = 0;
   // output.innerHTML = '';
 }
-// Make UI buttons control type
+
 var button = document.getElementsByTagName('input');
 for (i=0;i<button.length;i++){
   button[i].addEventListener('click',function(){switchType(this.value)});
